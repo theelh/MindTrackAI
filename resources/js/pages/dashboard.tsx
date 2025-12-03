80df75abfaf73349629f763c01dd0c3311797c47
theelh <meroelhosni123@gmail.com>
2025-12-03 19:09:44 +0100

    updates

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -103,7 +103,7 @@ export default function Dashboard({ quotes }: Props) {
                             href="/emotion/analytics"
                             className="hover:scale-[1.09] hover:border-indigo-400 hover:shadow-xl hover:bg-gradient-to-tl hover:from-black hover:via-indigo-950 hover:to-indigo-600 hover:shadow-black/40 hover:text-white transition-all duration-500 bg-gradient-to-tr shadow-lg shadow-white text-[15px] cursor-pointer mt-5 rounded-md flex items-center justify-center border font-semibold border-gray-300 px-5 py-2 from-gray-200 via-white to-gray-200"
                             >
-                            View Analytics
+                            View Analytics 
                             <ChartNoAxesCombinedIcon className="w-5 h-5 ml-2" />
                         </a>
                     </div>
